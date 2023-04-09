e4263cda60509fefbdfca3953034c1879a207c44
Mandeep Singh Mahar <maharmandeep2002@gmail.com>
2023-04-09 16:51:41 +0530

    person mail judne k badd

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import { Person,Mail } from '@mui/icons-material';
 
-export default function Topbar(){
-    return(
-        <div className="topbar">
-            
-        </div>
-    );
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+           <div className="left">
+            <a href="#intro" className="logo">genious.</a>
+             
+             <div className="container">
+              <Person></Person>
+              <span>+916399736067</span>
+             </div>
+             <div className="container">
+                  <Mail></Mail>
+                  <span>maharmandeep2000@gmail.com</span>
+             </div>
+           
+            </div>
+        <div className="right">mandeep</div>
+      </div>
+    </div>
+  );
 }
